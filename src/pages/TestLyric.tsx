7f05b33cb29b171ac6bf16e8f137b946b7d21ba1
TianCheng Tang <tiancheng.ext@1m.app>
2024-01-04 18:40:42 +0800

    fix: update icons

--- src/pages/TestLyric.tsx
+++ src/pages/TestLyric.tsx
@@ -1,11 +1,14 @@
 import "./test-lyric.css";
-import MusicLayoutHeader from '../components/Music/layout/MusicLayoutHeader'
-import MusicLayoutSlider from '../components/Music/layout/MusicLayoutSlider'
-import SanLyric from '../components/Music/common/SanLyric'
+import MusicLayoutHeader from "../components/Music/layout/MusicLayoutHeader";
+import MusicLayoutSlider from "../components/Music/layout/MusicLayoutSlider";
+import SanLyric from "../components/Music/common/SanLyric";
+import {
+  IoPauseCircleOutline,
+  IoPlaySkipBackOutline,
+  IoPlaySkipForwardOutline,
+} from "react-icons/io5";
 
 const TestLyric = () => {
-
-
   return (
     <div className="layout">
       <MusicLayoutHeader />
@@ -14,13 +17,20 @@ const TestLyric = () => {
         <MusicLayoutSlider />
 
         <div className="layout-container">
-         <div className="layout-new-lyric box-shadow">
-          <SanLyric />
-        </div>
-          <div className="box-shadow layout-control"></div>
-        </div>
+          <div className="layout-new-lyric box-shadow">
+            <SanLyric />
+          </div>
+          <div className="box-shadow layout-control">
+            <div className="control-box">
+              <div className="control-btn">
+                <IoPlaySkipBackOutline fontSize={24} fontWeight={600} />
+              </div>
 
-
+              <IoPauseCircleOutline fontSize={48} fontWeight={600} />
+              <IoPlaySkipForwardOutline fontSize={24} fontWeight={600} />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
